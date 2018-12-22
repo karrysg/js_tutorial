c685089562eae18314461c1f0c5a26ede3bc78bd
George Karrys <karrysg@earthlink.net>
2018-12-22 12:30:57 -0700

    modify day.js

--- day.js
+++ day.js
@@ -5,10 +5,7 @@ function dayName(date) {
                          "Thursday", "Friday", "Saturday"];
   return daysOfTheWeek[date.getDay()];
 }
-<<<<<<< HEAD
-=======
 
 function greeting(date){
   return `Hello, world! Happy ${daysOfTheWeek(now)}.`;
 }
->>>>>>> f908fc529cb927d5922f28a5395fd9dae47d147f
