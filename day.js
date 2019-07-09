6df3d0a24d64ab3b17830b1a23a7fe4fc2114d81
George Karrys <karrysg@earthlink.net>
2019-07-09 08:28:11 -0700

    unspecified changes

--- day.js
+++ day.js
@@ -1,11 +1,12 @@
 // Returns the day of the week for the given date.
 // Returns the day of the week for the given date.
 function dayName(date) {
-  const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday",
-                         "Thursday", "Friday", "Saturday"];
-  return daysOfTheWeek[date.getDay()];
+	const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday",
+		"Thursday", "Friday", "Saturday"
+	];
+	return daysOfTheWeek[date.getDay()];
 }
 
-function greeting(date){
-  return `Hello, world! Happy ${daysOfTheWeek(now)}.`;
+function greeting(date) {
+	return `Hello, world! Happy ${daysOfTheWeek(now)}.`;
 }
